4fe4093b4a08288e09a8d118137fbdd0edc99ccf
papaschloss <11808509+papaschloss@users.noreply.github.com>
2020-08-02 12:39:56 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh	
 set -e
 
+apt-get update && apt-get upgrade 
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
 
